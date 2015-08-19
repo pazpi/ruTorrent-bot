61039957e4f044b40edbb046895dd9b46f5fef77
pazpi <pasettodavide@gmail.com>
2015-08-19 22:07:02 +0200

    Change host address to pubblic one

--- bot.py
+++ bot.py
@@ -35,7 +35,7 @@ def main():
 
 
 def magnet(text):
-    url = 'http://192.168.1.190/ruTorrent/php/addtorrent.php?url=' + text
+    url = 'http://pazpi.noip.me:8080/ruTorrent/php/addtorrent.php?url=' + text
     # Test ArchLinux ISO
     #url = 'http://192.168.1.190/ruTorrent/php/addtorrent.php?url=' + 'magnet:?xt=urn:btih:828e86180150213c10677495565baef6b232dbdd&dn=archlinux-2015.08.01-dual.iso&tr=udp://tracker.archlinux.org:6969&tr=http://tracker.archlinux.org:6969/announce'
     #data = urllib.urlparse(url)
