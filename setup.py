02ac0e2250257d121e6627ab2220cc818095f525
pazpi <pasettodavide@gmail.com>
2015-09-17 22:01:33 +0200

    Add info about program

---
+++ setup.py
@@ -0,0 +1,10 @@
+from distutils.core import setup
+
+setup(name='ruTorrent-bot',
+      version='0.1',
+      description='telegram bot that add torrent to a ruTorrent interface',
+      author='@pazpi, @martinotu',
+      author_email='pasettodavide@gmail.com',
+      url='https://www.github.com/pazpi/ruTorrent-bot',
+      packages=['distutils', 'distutils.command'],
+     )
