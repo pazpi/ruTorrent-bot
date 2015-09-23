3c67575dfdcbd518bdcdf262c9b531d7f44ff68c
pazpi <pasettodavide@gmail.com>
2015-09-23 11:03:16 +0200

    New file for handle first time setup

---
+++ init.py
@@ -0,0 +1,33 @@
+"""
+Init file
+
+/start -> config bot -> host & port
+
+- Custom keyboard
+"""
+
+import telegram
+import telegram_bot
+import config
+import bot
+
+def config_start(chat_id):
+    """keyboard_host_port = [[ "HOST", "PORT", "EXIT"]]
+    reply_markup = telegram.ReplyKeyboardMarkup(keyboard_host_port)
+    bot.sendMessage(chat_id=chat_id, text="Choose wisely", reply_markup=reply_markup)"""
+    bot.setKeyboard("HOST", "PORT", "EXIT")
+    if (bot.getUpdates(offset=LAST_UPDATE_ID).message.text == "HOST"):
+        setHost()
+    elif (bot.getUpdates(offset=LAST_UPDATE_ID).message.text == "PORT"):
+        setPort()
+    elif (bot.getUpdates(offset=LAST_UPDATE_ID).message.text == "EXIT"):
+        return
+    else:
+        return
+    
+def setHost():
+    
+    return
+    
+def setPort():
+    return
