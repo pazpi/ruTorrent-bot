217fd0cd8658550b701a4ca923863d22101e3004
pazpi <pasettodavide@gmail.com>
2015-09-24 09:29:35 +0200

    Fixes config file import

--- telegram_bot.py
+++ telegram_bot.py
@@ -1,20 +1,6 @@
 import telegram
-import config.TOKEN as token
+import config
 
-# def BotDef():
-#     bot = telegram.Bot(token)
-#     LAST_UPDATE_ID = bot.getUpdates()[-1].update_id
-#     
-#     updates = bot.getUpdates(offset=LAST_UPDATE_ID)
-#     if(not updates):
-#         logger.error("Couldn't get updates")
-#         return
-#     for update in updates:
-#         command = update.message.text
-#         chat_id = update.message.chat.id
-#         update_id = update.update_id
-#         answer = ''
-#         init.config_start(chat_id)
         
 class Bot:
     bot = telegram.Bot("116051020:AAHrxECX7pDpDuQgTMYbamGVKLQCPxpVkKA")
