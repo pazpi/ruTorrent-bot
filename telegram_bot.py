cdd51b67dbb5964fdff1b385472be8cc5db0651a
pazpi <pasettodavide@gmail.com>
2015-09-24 11:46:41 +0200

    Add update methot to bot class

--- telegram_bot.py
+++ telegram_bot.py
@@ -1,15 +1,22 @@
+# telegram_bot.py
+import logging
 import telegram
 import config
 
-        
+module_logger = logging.getLogger(__name__)
+
 class Bot:
-    bot = telegram.Bot("116051020:AAHrxECX7pDpDuQgTMYbamGVKLQCPxpVkKA")
+    bot = telegram.Bot(config.TOKEN)
     command = ''
     chat_id = ''
     update_id = ''
-    LAST_UPDATE_ID = '' 
+    LAST_UPDATE_ID = ''
     def __init__(self):
+        self.logger = logging.getLogger("telegram_bot.Bot")
+        self.logger.info("Bot creation")
         self.LAST_UPDATE_ID = self.bot.getUpdates()[-1].update_id
+    
+    def Update(self):
         updates = self.bot.getUpdates(offset=self.LAST_UPDATE_ID)
         for update in updates:
             self.command = update.message.text
