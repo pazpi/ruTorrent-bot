7a0e184ae44a045a08b80f6dc908d620656a8f3b
martinotu94 <martino.turrina@gmail.com>
2015-09-30 09:24:15 +0200

    cleanup

--- handleTorrent.py
+++ handleTorrent.py
@@ -21,8 +21,5 @@ def Hash2Magnet(hash):
 
 
 def addMagnet(torrent):
-    url =  "http://pazpi.noip.me:8080/" + 'ruTorrent/php/addtorrent.php?url=' + torrent #pazpi.ecc to replace with the setting from the user
-    # print(url)
-    # Test ArchLinux ISO
-    # url = 'http://192.168.1.190/ruTorrent/php/addtorrent.php?url=' + 'magnet:?xt=urn:btih:828e86180150213c10677495565baef6b232dbdd&dn=archlinux-2015.08.01-dual.iso&tr=udp://tracker.archlinux.org:6969&tr=http://tracker.archlinux.org:6969/announce'
+    url =  "http://pazpi.noip.me:8080/" + 'ruTorrent/php/addtorrent.php?url=' + torrent #http://pazpi.ecc to replace with the setting from the user
     requests.post(url, auth=HTTPBasicAuth("",""))
