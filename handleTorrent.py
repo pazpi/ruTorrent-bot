9b73ed7a5c72e91c5925fa42f490e1ade4675492
pazpi <pasettodavide@gmail.com>
2016-01-14 12:41:27 +0100

    PEP8 recommendation

--- handleTorrent.py
+++ handleTorrent.py
@@ -1,26 +1,26 @@
 # handleTorrent.py
 # function to manipulate all the torrent part
-
-import logging, coloredlogs
-#import auxiliary_module
-from logging.handlers import RotatingFileHandler
+#
+# import logging, coloredlogs
+# import auxiliary_module
+# from logging.handlers import RotatingFileHandler
 # requests module for basic http post
 import requests
 from requests.auth import HTTPBasicAuth
 # telegram module for easy work with bot conf
-#import telegram
+# import telegram
 # file used to store sensible data, like API key
-import config
-#import init
-import botDef
+# import config
+# import init
+# import botDef
 
 
-def Hash2Magnet(hash):
-    magnet = ''
+def hash2magnet(hash):
     magnet = "magnet:?xt=urn:btih:" + hash[2:-2]
     return magnet
 
 
-def addMagnet(torrent):
-    url =  "http://pazpi.noip.me:8080/" + 'ruTorrent/php/addtorrent.php?url=' + torrent #http://pazpi.ecc to replace with the setting from the user
-    requests.post(url, auth=HTTPBasicAuth("",""))
+def addmagnet(torrent):
+    # http://pazpi.ecc to replace with the setting from the user
+    url = "http://pazpi.noip.me:8080/" + 'ruTorrent/php/addtorrent.php?url=' + torrent
+    requests.post(url, auth=HTTPBasicAuth("", ""))
