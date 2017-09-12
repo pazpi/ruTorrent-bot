06e685ce30df5ec95cde9d856f45fcc16a0769a2
pazpi <pasettodavide@gmail.com>
2017-09-12 11:18:04 +0200

    Change some varialbe

--- setup.py
+++ setup.py
@@ -1,15 +1,10 @@
 # setup.py
 from distutils.core import setup
 
-with open('requirements.txt') as f:
-    requirements = f.read().splitlines()
-
 setup(name='ruTorrent-bot',
       version='0.1',
       description='telegram bot that add torrent to a ruTorrent interface',
       author='@pazpi, @martinotu',
       author_email='pasettodavide@gmail.com',
-      url='https://www.github.com/pazpi/ruTorrent-bot',
-      # install_requires=requirements,
-      packages=['python-telegram-bot', 'requests'],
+      url='https://www.gitlab.com/pazpi/ruTorrent-bot',
       )
